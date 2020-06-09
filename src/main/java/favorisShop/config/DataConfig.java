b7f84cb24f467336f2aa0897d4aa15d8e16392ee
vova99 <mr.oleksiyenko@gmail.com>
2020-06-09 16:01:39 +0300

    sdsd

--- src/main/java/favorisShop/config/DataConfig.java
+++ src/main/java/favorisShop/config/DataConfig.java
@@ -18,73 +18,73 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories("favorisShop.dao")
+//@Configuration
+//@EnableTransactionManagement
+//@EnableJpaRepositories("favorisShop.dao")
 public class DataConfig {
 
-    @Autowired
-    Environment e;
-
-
-    @Bean
-    public DataSource dataSource(){
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
-
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
-
-//        hikariConfig.setJdbcUrl((dbUrl!=null)?dbUrl:"jdbc:mysql://localhost:3306/favoris");
-//        hikariConfig.setUsername((username!=null)?username:"root");
-//        hikariConfig.setPassword((password!=null)?password:"password");
-
-        hikariConfig.setJdbcUrl("mysql://z73xojno142c8aob:mqf59cacfbjn8w0s@b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/wr886dgckc9iiaiv");
-        hikariConfig.setUsername("z73xojno142c8aob");
-        hikariConfig.setPassword("mqf59cacfbjn8w0s ");
-
-        hikariConfig.setMaximumPoolSize(5);
-        hikariConfig.setConnectionTestQuery("SELECT 1");
-        hikariConfig.setPoolName("springHikariCP");
-
-        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
-        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
-
-        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-
-
-        return dataSource;
-
-    }
-
-    @Bean
-    public JpaVendorAdapter vendorAdapter(){
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(true);
-        vendorAdapter.setDatabase(Database.MYSQL);
-        return vendorAdapter;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource());
-        factoryBean.setJpaVendorAdapter(vendorAdapter());
-        factoryBean.setPackagesToScan("favorisShop.entity");
-        Properties properties = new Properties();
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        factoryBean.setJpaProperties(properties);
-        return factoryBean;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
+//    @Autowired
+//    Environment e;
+//
+//
+//    @Bean
+//    public DataSource dataSource(){
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = System.getenv("JDBC_DATABASE_USERNAME");
+//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//
+//        HikariConfig hikariConfig = new HikariConfig();
+//        hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+//
+////        hikariConfig.setJdbcUrl((dbUrl!=null)?dbUrl:"jdbc:mysql://localhost:3306/favoris");
+////        hikariConfig.setUsername((username!=null)?username:"root");
+////        hikariConfig.setPassword((password!=null)?password:"password");
+//
+//        hikariConfig.setJdbcUrl("mysql://z73xojno142c8aob:mqf59cacfbjn8w0s@b8rg15mwxwynuk9q.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/wr886dgckc9iiaiv");
+//        hikariConfig.setUsername("z73xojno142c8aob");
+//        hikariConfig.setPassword("mqf59cacfbjn8w0s ");
+//
+//        hikariConfig.setMaximumPoolSize(5);
+//        hikariConfig.setConnectionTestQuery("SELECT 1");
+//        hikariConfig.setPoolName("springHikariCP");
+//
+//        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+//        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+//        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+//
+//        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+//
+//
+//        return dataSource;
+//
+//    }
+//
+//    @Bean
+//    public JpaVendorAdapter vendorAdapter(){
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        vendorAdapter.setShowSql(true);
+//        vendorAdapter.setDatabase(Database.MYSQL);
+//        return vendorAdapter;
+//    }
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+//        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+//        factoryBean.setDataSource(dataSource());
+//        factoryBean.setJpaVendorAdapter(vendorAdapter());
+//        factoryBean.setPackagesToScan("favorisShop.entity");
+//        Properties properties = new Properties();
+//        properties.put("hibernate.hbm2ddl.auto", "update");
+//        factoryBean.setJpaProperties(properties);
+//        return factoryBean;
+//    }
+//
+//    @Bean
+//    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+//        return new JpaTransactionManager(entityManagerFactory);
+//    }
 
 
 }
