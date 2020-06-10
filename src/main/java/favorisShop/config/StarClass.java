2a3f71d919f46c6a34c58424e5bbeefdbae1a948
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 11:01:52 +0300

    sdsd

---
+++ src/main/java/favorisShop/config/StarClass.java
@@ -0,0 +1,11 @@
+package favorisShop.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarClass {
+    public static void main(String[] args) {
+        SpringApplication.run(StarClass.class,args);
+    }
+}
