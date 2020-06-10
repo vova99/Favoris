134c23e2e5e1a33dd0ab8aa86739fc208ec8a66a
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 15:07:18 +0300

    sdsd

--- src/main/java/favorisShop/StarClass.java
+++ src/main/java/favorisShop/StarClass.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan("*")
 public class StarClass {
 
     public static void main(String[] args) {
