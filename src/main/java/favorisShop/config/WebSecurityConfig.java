e8248b4b57421029e5efbb9dc4d2cd6e014673e3
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 15:02:18 +0300

    sdsd

--- src/main/java/favorisShop/config/WebSecurityConfig.java
+++ src/main/java/favorisShop/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan("favorisShop.controller.*")
+@ComponentScan("favorisShop.*")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
        @Autowired
