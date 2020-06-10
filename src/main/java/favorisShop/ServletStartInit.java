fbb6a759edf61ee7fde2fbd8769e154773202aed
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 17:41:27 +0300

    sdsd

---
+++ src/main/java/favorisShop/ServletStartInit.java
@@ -0,0 +1,8 @@
+package favorisShop;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ServletStartInit extends SpringBootServletInitializer {
+}
--- src/main/java/favorisShop/config/WebConfig.java
+++ src/main/java/favorisShop/config/WebConfig.java
@@ -7,10 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.io.File;
@@ -27,7 +24,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public MultipartResolver multipartResolver(){
         return new StandardServletMultipartResolver();
     }
-
 
 
     @Bean
