13dd17883f3e4f586be46a6ab84f0e16925e62f8
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 17:44:55 +0300

    sdsd

--- src/main/java/favorisShop/StarClass.java
+++ src/main/java/favorisShop/StarClass.java
@@ -1,9 +1,14 @@
 package favorisShop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -13,18 +18,18 @@ public class StarClass {
     public static void main(String[] args) {
         SpringApplication.run(StarClass.class,args);
     }
-//    @Bean
-//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//        return args -> {
-//
-//            System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                System.out.println(beanName);
-//            }
-//
-//        };
-//    }
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
 }
