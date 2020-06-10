b017020e369755bce2e7c07952370a8c0e561fa9
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 15:26:33 +0300

    sdsd

--- src/main/java/favorisShop/StarClass.java
+++ src/main/java/favorisShop/StarClass.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"favorisShop.config","favorisShop.controller"})
+@ComponentScan(basePackages = {"favorisShop.config","favorisShop.controller","favorisShop.dao"})
 public class StarClass {
 
     public static void main(String[] args) {
