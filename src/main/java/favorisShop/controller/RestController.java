f79e92cef36bd5c5492672550f0d00e680578950
vova99 <mr.oleksiyenko@gmail.com>
2020-06-10 15:31:41 +0300

    sdsd

--- src/main/java/favorisShop/controller/RestController.java
+++ src/main/java/favorisShop/controller/RestController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-    @RequestMapping("/login")
-    public String index() {
-        return "login";
-    }
+//    @RequestMapping("/login")
+//    public String index() {
+//        return "login";
+//    }
 
 
 }
