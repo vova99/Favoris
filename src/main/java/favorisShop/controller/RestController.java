52c45a4071463a5f2834ccbe57c6221e4afaf9cb
vova99 <mr.oleksiyenko@gmail.com>
2020-06-12 17:01:58 +0300

    sdsd

--- src/main/java/favorisShop/controller/RestController.java
+++ src/main/java/favorisShop/controller/RestController.java
@@ -2,9 +2,14 @@ package favorisShop.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-
+    @RequestMapping("/error")
+    @ResponseBody
+    public String getErrorPath() {
+        return "error";
+    }
 
 }
