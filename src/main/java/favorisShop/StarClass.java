291fa597d3b4d13eaf09635a8c727bdfb0348229
vova99 <mr.oleksiyenko@gmail.com>
2020-06-14 12:14:04 +0300

    sdsd

--- src/main/java/favorisShop/StarClass.java
+++ src/main/java/favorisShop/StarClass.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,12 +19,17 @@ import java.util.Arrays;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"favorisShop.config","favorisShop.controller","favorisShop.dao"})
-public class StarClass {
+public class StarClass extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StarClass.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StarClass.class,args);
     }
+
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
