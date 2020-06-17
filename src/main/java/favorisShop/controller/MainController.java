5903d73fd61e0fa8c8d57719af361f066bf7bf61
vova99 <mr.oleksiyenko@gmail.com>
2020-06-17 19:55:06 +0300

    sdsd

--- src/main/java/favorisShop/controller/MainController.java
+++ src/main/java/favorisShop/controller/MainController.java
@@ -22,7 +22,7 @@ public class MainController //implements ErrorController
     UserService userService;
     @GetMapping("/registration")
     public String getRegistry(){
-        return "error";
+        return "registration";
     }
 
     @GetMapping("/index")
