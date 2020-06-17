ec0662bcde1e615fe308077a0a0dc470a6ff460d
vova99 <mr.oleksiyenko@gmail.com>
2020-06-17 19:23:30 +0300

    sdsd

--- src/main/java/favorisShop/controller/SecurityController.java
+++ src/main/java/favorisShop/controller/SecurityController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.persistence.Enumerated;
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class SecurityController {
     UserService userService;
 
     @GetMapping("/registration")
+    @ResponseBody
     public String getRegistry(){
         return "registration";
     }
